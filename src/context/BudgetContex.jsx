c0a1448e50bfd04e5e6376c20212fb0712efecd0
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-11 14:45:10 +0100

    Feat: BudgetContext functions

--- src/context/BudgetContex.jsx
+++ src/context/BudgetContex.jsx
@@ -1,14 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 const BudgetContext = createContext();
 
-function BudgetProvider(){
+
+function BudgetProvider({ children }) {
+    const [budgetMode, setBudgetMode] = useState("false");
+
+    function isBudgetMode(id) {
+        return budgetMode.includes(id);
+    }
+
+    function addBudgetMode(id) {
+        setBudgetMode((current) => [...current, id]);
+    }
+
+    function deleteBudgetMode(id) {
+        setBudgetMode((current) => current.filter((budgetModeId) => id !== budgetModeId));
+    }
+
+    const contextValue = {
+        isBudgetMode,
+        addBudgetMode,
+        deleteBudgetMode
+    };
+
+
     return (
-    <BudgetContext.Provider value={contextValue}>
-   
-    </BudgetContext.Provider>
-  );
+        <BudgetContext.Provider value={contextValue}>
+            {children}
+        </BudgetContext.Provider>
+    );
 
 }
 export { BudgetProvider }
